18655b7346d856bc7add9b8a60028220a6429b0b
Hoai An Nguyen <anamatxcova@gmail.com>
2025-04-25 18:27:27 -0700

    RAG chatbot set up

---
+++ dermasight-backend/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot AI Project',
+    version= '0.0.0',
+    author= 'Hoai An Nguyen',
+    packages= find_packages(),
+    install_requires = []
+
+)
