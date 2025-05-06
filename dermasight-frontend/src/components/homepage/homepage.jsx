bd6fce50141d8da34940232aa28e8515ffe863f3
jmd0711 <2jmd0711@gmail.com>
2025-05-06 15:39:12 -0700

    homepage text

--- dermasight-frontend/src/components/homepage/homepage.jsx
+++ dermasight-frontend/src/components/homepage/homepage.jsx
@@ -1,12 +1,25 @@
-import { Button, Container, Form, FloatingLabel } from 'react-bootstrap'
-
+import { Button, Container, Form, FloatingLabel, Row, Col } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import './homepage.css'
 const HomePage = () => {
+  const navigate=useNavigate()
+  const onStarted = () => {
+    navigate('/upload')
+  }
+
   return (
     <Container fluid className='main-page'>
-      <div className='main-body h-100'>
-        <h3 className="title text-dark mb-3">HomePage</h3>
-        
+      <div className='main-body'>
+        <Row className='homepage-content'>
+            <Col>
+            <h3 className="title text-dark mb-3">DermaSight</h3>
+            <h4>Spot The Signs,</h4>
+            <h4>Protect Your Skin.</h4>
+            <Button variant="primary" onClick={onStarted}>Get Started</Button>
+          </Col>
+        </Row>
       </div>
+
     </Container>
   )
 }
